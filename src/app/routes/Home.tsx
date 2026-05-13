1118800a2bef0fbe788432f85f4a4d9400209ed3
Sinval Luz <contato.sinvalluz@gmail.com>
2026-05-13 10:33:32 -0300

    feat(ui): add shadcn/ui support and theme configuration

--- src/app/routes/Home.tsx
+++ src/app/routes/Home.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomeRoute() {
-  return <h1>Home Route - Quyro Tech</h1>;
+  return (
+    <Button>
+      <h1>Home Route - Quyro Tech</h1>
+    </Button>
+  );
 }
